be9d76f0761a49beebed238d84d4b4bd8866ea78
Xiaoqiang Guo <guoxiaoq@gmail.com>
2019-10-10 15:43:02 +0800

    fix setup

--- src/Library/setup.fsx
+++ src/Library/setup.fsx
@@ -5,15 +5,15 @@ open Commod.Utils
 let addPath dir =
     let entries = 
         Environment.GetEnvironmentVariable("Path").Split(';')
+        |> set
+        |> Set.toArray
         |> Array.filter( fun x -> x <> dir)
         |> Array.filter( fun x -> x <> @"C:\Commodities\bin")
         |> Array.append( [|dir|] ) //add to the front
     let path = String.Join(";", entries)
     Environment.SetEnvironmentVariable("Path", path, EnvironmentVariableTarget.User)
 
-let dir = Environment.GetEnvironmentVariable("USERPROFILE") 
-            + @"\OneDrive - Pavilion Energy\Commodities\bin"
-            +/ "OneDrive - Pavilion Energy" 
+let dir = Environment.GetEnvironmentVariable("OneDrive") 
             +/ "Commodities" 
             +/ "bin"
 Environment.SetEnvironmentVariable("COMMODITIES", dir, EnvironmentVariableTarget.User)

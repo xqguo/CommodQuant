516a421422cf27a49f2c4e9d28eb91153d5a56ea
Xiaoqiang GUO <guoxiaoq@gmail.com>
2021-02-09 15:44:40 +0800

    change mc vs choi test, using 17/2.

--- src/Test/TestMonteCarlo.fs
+++ src/Test/TestMonteCarlo.fs
@@ -126,13 +126,15 @@ let testSpreadChoivsMC fa fb k t1 t2 v1 v2 rho nf1 nf2 callput =
     let t2 = DenseVector.init nf2 ( fun i -> float (i+1)/250. + t2 )
     let fw1 = DenseVector.create nf1 1./(float nf1) 
     let fw2 = DenseVector.create nf2 1./(float nf2) 
-    let rho = max (min (rho/5.0) 0.9) -0.9  //correlation between long/short fixing
+    //let rho = max (min (rho/5.0) 0.9) -0.9  //correlation between long/short fixing
     let (c,std) = spreadMC f1 fw1 t1 v1 f2 fw2 t2 v2 k rho callput num
-    let choi,_ = optionChoi2AssetN f1 fw1 t1 v1 f2 fw2 t2 v2 k rho callput [7;2;2] 
-    nearstr choi c (std * 3.0 + 1E-2) "Choi vs mc" //choi and mc close
+    let choi,_ = optionChoi2AssetN f1 fw1 t1 v1 f2 fw2 t2 v2 k rho callput [17;2] 
+    nearstr choi c (std * 3.0 + 1E-6) "Choi vs mc" //choi and mc close
 
-[<Property( MaxTest=20, Verbose = true, EndSize = 100, Arbitrary = [| typeof<PositiveFloat>|] )>]
-let ``test spread option Choi vs MC`` fa fb k t1 t2 v1 v2 (NormalFloat rho) (PositiveInt nf1) (PositiveInt nf2) callput = 
+[<Property( MaxTest=200, Verbose = true, EndSize = 100, Arbitrary = [| typeof<PositiveFloat>;typeof<MyGenerator>|] )>]
+let ``test spread option Choi vs MC`` fa fb k t1 t2 v1 v2 (Corr rho) (PositiveInt nf1) (PositiveInt nf2) callput = 
+    let t1 = min t1 4.0
+    let t2 = min t2 4.0
     testSpreadChoivsMC fa fb k t1 t2 v1 v2 rho nf1 nf2 callput
 
 //[<Property( MaxTest=1, Verbose = true, EndSize = 100, Arbitrary = [| typeof<PositiveFloat>|] )>]

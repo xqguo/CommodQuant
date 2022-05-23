36b6aa79feaca612d751ee6f4210bc9a078427ec
Xiaoqiang GUO <guoxiaoq@gmail.com>
2022-05-23 14:56:26 +0800

    add nbp

--- src/Library/icecontracts.fsx
+++ src/Library/icecontracts.fsx
@@ -14,6 +14,7 @@ let sources =
        "BRT", @"https://www.theice.com/api/productguide/spec/219/expiry/csv", @"https://www.theice.com/api/productguide/spec/218/expiry/csv"
        "TTF", @"https://www.theice.com/api/productguide/spec/27996665/expiry/csv", @"https://www.theice.com/api/productguide/spec/71085679/expiry/csv"
        "NG", @"https://www.theice.com/api/productguide/spec/6590258/expiry/csv", @"https://www.theice.com/api/productguide/spec/79347917/expiry/csv"
+       "NBP", @"https://www.theice.com/api/productguide/spec/910/expiry/csv", @"https://www.theice.com/api/productguide/spec/71085728/expiry/csv"
     ]
 let saveIceFutDates f (futurl:string) (opturl:string)= 
     let fut = iceFut.Load futurl

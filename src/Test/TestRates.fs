88dc976067d779bc28f03459262025ea7a00ab7e
Xiaoqiang GUO <guoxiaoq@gmail.com>
2022-08-15 17:23:38 +0800

    rate test

--- src/Test/TestRates.fs
+++ src/Test/TestRates.fs
@@ -6,13 +6,11 @@ open FsCheckTypes
 open QLNet
 
 //[<Property (Verbose =true) >]
-[<Property( Arbitrary = [| typeof<ValidDate>|])>]
+[<Property( EndSize = 1, Arbitrary = [| typeof<ValidDate>|])>]
 let ``test discount act365 exp`` (NormalFloat r ) (d1:DateTime) (d2:DateTime) = 
-    ( d1 <= d2 ) ==> lazy(
-        let r = max (min r 1.0 ) -1.0 //limit r in +/- 100% to avoid inf
-        let days = (d2 - d1).TotalDays
-        let rate = InterestRate(r, Actual365Fixed(), Compounding.Continuous, Frequency.NoFrequency )
-        let disc = rate.discountFactor( Date(d1), Date(d2))
-        let disc' = exp( -r * days/365.)
-        near disc disc' ( disc * 1E-10)
-        )
+    let [d1;d2] = [d1 ; d2] |> List.sort
+    let days = (d2 - d1).TotalDays
+    let rate = InterestRate(r, Actual365Fixed(), Compounding.Continuous, Frequency.NoFrequency )
+    let disc = rate.discountFactor( Date(d1), Date(d2))
+    let disc' = exp( -r * days/365.)
+    near disc disc' ( disc * 1E-10)

83bad8dfb4b4f867bf967cbff7f359bc5d9bba10
Xiaoqiang GUO <guoxiaoq@gmail.com>
2022-08-16 16:44:26 +0800

    tune tests

--- src/Test/TestRates.fs
+++ src/Test/TestRates.fs
@@ -8,7 +8,8 @@ open QLNet
 //[<Property (Verbose =true) >]
 [<Property( EndSize = 1, Arbitrary = [| typeof<ValidDate>|])>]
 let ``test discount act365 exp`` (NormalFloat r ) (d1:DateTime) (d2:DateTime) = 
-    let [d1;d2] = [d1 ; d2] |> List.sort
+    //let [d1;d2] = [d1 ; d2] |> List.sort
+    let (d1,d2) = if d1 < d2 then (d1,d2) else (d2,d1)
     let days = (d2 - d1).TotalDays
     let rate = InterestRate(r, Actual365Fixed(), Compounding.Continuous, Frequency.NoFrequency )
     let disc = rate.discountFactor( Date(d1), Date(d2))
